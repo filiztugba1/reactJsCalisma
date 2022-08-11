56d60edce96534ca2da88ed8a975628c6566757c
fcurukcu <filiz.curukcu@ieu.edu.tr>
2022-08-11 17:34:59 +0300

    resim apiye bağlandı unsplash

---
+++ resim/src/components/image/ImageList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class ImageList extends Component {
+  render() {
+    return (
+      <div>
+        Hallo
+      </div>
+    )
+  }
+}
+export default ImageList;
