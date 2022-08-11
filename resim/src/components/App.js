a27dc2b4d10d035fc8d580de4d0113444ba86aa3
fcurukcu <filiz.curukcu@ieu.edu.tr>
2022-08-11 16:30:13 +0300

    apiye_bağlama işlemleri

---
+++ resim/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar/SearchBar';
+import './app.css';
+
+class App extends Component {
+
+    onSearchImage=search=>{
+        console.log('App'+search);
+    }
+
+    render() {
+        return (
+            <div className='app-container'>
+                <SearchBar onSearchImage={this.onSearchImage}/>
+            </div>
+        );
+    }
+}
+
+export default App;
