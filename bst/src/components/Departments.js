a568aa8775df8a10fa4544a6b1aa4d33f0bbc3a4
fcurukcu <filiz.curukcu@ieu.edu.tr>
2022-08-23 16:07:20 +0300

    api

--- bst/src/components/Departments.js
+++ bst/src/components/Departments.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react'
-
+import {Table} from 'react-bootstrap';
+import { AddDepModal } from './AddDepModal';
+import {Button,ButtonToolbar} from 'react-bootstrap'; 
 export class Departments extends Component {
+    
+  constructor(props)
+  {
+    super(props);
+    this.state={
+        deps:[],
+        addModalShow:false
+    }
+  }
+
+  componentDidMount(){
+    this.refleshlist();
+  }
+  refleshlist()
+  {
+    fetch('http://localhost:63874/api/Departments')
+    .then((response) => response.json())
+    .then((data) =>
+        this.setState({
+            deps:data
+        })
+    );
+  }
+
   render() {
+    const{deps}=this.state;
+    let addModalClose=()=>this.setState({addModalShow:false})
     return (
-      <div className='mt-5 d-flex justify-content-left'>
-        <h3>Welcome to departments page</h3>
-      </div>
+     <div>
+         <Table className='mt-4' striped bordered hover size="sm">
+            <thead>
+                <tr>
+                    <th>DepartmentID</th>
+                    <th>DepartmentName</th>
+                </tr>
+            </thead>
+            <tbody>
+                {deps.map(dep=>
+                    <tr key={dep.ID}>
+                       <td>{dep.ID}</td>
+                       <td>{dep.DepartmentName}</td>
+                   </tr>
+                   )}
+             
+            </tbody>
+      </Table>
+      <ButtonToolbar>
+        <Button onClick={()=>this.setState({addModalShow:true})}>
+            Add Department
+        </Button>
+      </ButtonToolbar>
+      <AddDepModal show={this.state.addModalShow} onHide={addModalClose}/>
+     </div>
     )
   }
 }
