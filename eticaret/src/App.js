cc23162c275bb95ca284d139b90acdb90ab184f1
Şeymanur Çürükcü <seymanurcurukcu@gmail.com>
2022-11-05 15:35:25 +0300

    Temizlendi

--- eticaret/src/App.js
+++ eticaret/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          testttt deneme sdsd  zxcfxcs
-        </p>
+      
         <a
           className="App-link"
           href="https://reactjs.org"
